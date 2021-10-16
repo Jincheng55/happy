5b7117918f49baf965130ca3dcd5f85a4d54b57b
金城武 <469490228@qq.com>
2021-10-16 13:20:12 +0800

    first commit

---
+++ src/page/item/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <itemcontainer father-component="item"></itemcontainer>
+  </div>
+</template>
+
+<script>
+import itemcontainer from '@/components/itemcontainer.vue';
+export default {
+  name: 'home',
+  components: {
+    itemcontainer
+  }
+};
+</script>
+
+<style></style>
